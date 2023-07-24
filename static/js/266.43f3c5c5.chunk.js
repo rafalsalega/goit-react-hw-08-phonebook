196fb02c42c3787eeac5e373c739065ebc713724
rafalsalega <105451060+rafalsalega@users.noreply.github.com>
2023-07-24 21:05:00 +0000

    Deploying to gh-pages from @ rafalsalega/goit-react-hw-08-phonebook@ba28b94d4e959e778e8dcf1466e97f5d1026a7e2 🚀

---
+++ static/js/266.43f3c5c5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(184),i={container:{minHeight:"calc(100vh - 500px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function l(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsxs)("h1",{style:i.title,children:["Hello!",(0,r.jsx)("br",{}),"Welcome to your Phonebook"," "]})})}}}]);
+//# sourceMappingURL=266.43f3c5c5.chunk.js.map
